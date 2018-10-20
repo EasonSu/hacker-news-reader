f4fecd3c2ddb58d8302733f3ca12180520e940b1
Eason <eason@17.media>
2018-10-21 02:46:31 +0800

    Implement item related API, actions and reducers

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -1,22 +1,50 @@
-import { createAction } from 'redux-actions';
+import { createActions } from 'redux-actions';
 import api from 'services/api';
+import { pageSize } from './constants';
 
-export const NEWEST_ID = 'NEWEST_ID';
+export const SET_NEWEST = 'SET_NEWEST';
+export const SET_ITEM = 'SET_ITEM';
 
-export const setNewestID = createAction(
-  NEWEST_ID,
-  ({ ids, isFetching = false }) => {
-    const payload = { isFetching };
-    if (ids) {
-      payload.ids = ids;
-    }
-    return payload;
-  }
-);
+export const { setNewest, setItem } = createActions({
+  [SET_NEWEST]: (data) => {
+    const { isFetching = false } = data;
+    return { ...data, isFetching };
+  },
+
+  [SET_ITEM]: item => ({
+    id: item.id,
+    item,
+  }),
+});
 
 export function fetchNewestList() {
   return (dispatch) => {
-    dispatch(setNewestID({ isFetching: true }));
-    api.getNewestIDs().then(ids => dispatch(setNewestID({ ids })));
+    dispatch(setNewest({ isFetching: true }));
+    api.getNewestIDs().then(ids => dispatch(setNewest({ ids })));
+  };
+}
+
+export function fetchNextPage() {
+  return (dispatch, getState) => {
+    const { ids, offset, isFetching } = getState().app.newest;
+
+    if (isFetching) {
+      return;
+    }
+
+    dispatch(setNewest({ isFetching: true }));
+
+    const nextOffset = Math.min(offset + pageSize, ids.length);
+    const nextIDs = ids.slice(offset, nextOffset);
+    const tasks = nextIDs.map((id) => { // eslint-disable-line arrow-body-style
+      return api.getItem(id)
+        .then((item) => {
+          dispatch(setItem(item));
+          return item;
+        });
+    });
+
+    Promise.all(tasks)
+      .then(() => dispatch(setNewest({ offset: nextOffset })));
   };
 }
--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -1,20 +1,51 @@
 import { handleActions } from 'redux-actions';
-import { NEWEST_ID } from './actions';
+import { SET_NEWEST, SET_ITEM } from './actions';
+
+export function findAvailableIndex(prevIndex, ids, itemDict) {
+  let nextIndex = prevIndex;
+  let item;
+  do {
+    nextIndex += 1;
+    item = itemDict[ids[nextIndex]];
+  } while (item);
+  return nextIndex - 1;
+}
 
 const defaultState = {
-  item: {},
+  itemDict: {},
   newest: {
     isFetching: false,
     ids: [],
+    offset: 0,
+    availableIndex: -1,
   },
 };
 
 export default handleActions({
-  [NEWEST_ID]: (state, { payload }) => ({
+  [SET_NEWEST]: (state, { payload }) => ({
     ...state,
     newest: {
       ...state.newest,
       ...payload,
     },
   }),
+
+  [SET_ITEM]: (state, { payload: { id, item } }) => {
+    const { ids, availableIndex: prevIndex } = state.newest;
+    const itemDict = {
+      ...state.itemDict,
+      [id]: item,
+    };
+
+    const availableIndex = findAvailableIndex(prevIndex, ids, itemDict);
+
+    return {
+      ...state,
+      newest: {
+        ...state.newest,
+        availableIndex,
+      },
+      itemDict,
+    };
+  },
 }, defaultState);
