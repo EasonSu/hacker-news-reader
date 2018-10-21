e0dbe2bce20a93b307774cbb642a76c8cc285d87
Eason <eason@17.media>
2018-10-21 22:37:28 +0800

    Change default port to 3886

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ setup(app, {
 
 const host = process.env.HOST || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = host || 'localhost';
-const port = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT || '3886', 10);
 
 // Start server
 app.listen(port, host, (err) => {
